2140b1a39822682bb017d03c769db54bc420d61d
Bruno Di Livio <bruno.dilivio@gmail.com>
2022-02-16 00:51:11 +0100

    Ajout du changement de mot de passe

--- Screen/Profil.js
+++ Screen/Profil.js
@@ -1,89 +1,112 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity, Modal, TextInput, Text} from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  Text,
+} from "react-native";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 import AdminButton from "../Components/AdminButton";
 import modalStyle from "./Modal.style";
 import { useState } from "react";
-import Circle from "../Components/Circle"
+import Api from "../Api";
+import Circle from "../Components/Circle";
 
 const Profil = (props) => {
-    const [modalPasswordVisible, setModalPasswordVisible] = useState(false);
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmedPassword, setConfirmedPassword] = useState("");
-    const [oldPassword, setOldPassword] = useState("");
+  const [modalPasswordVisible, setModalPasswordVisible] = useState(false);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
 
-    const resetModalNewPassword = () => {
-        setNewPassword("");
-        setOldPassword("");
-        setConfirmedPassword("");
-        setModalPasswordVisible(false);
-    }
+  const resetModalNewPassword = () => {
+    setNewPassword("");
+    setOldPassword("");
+    setConfirmedPassword("");
+    setModalPasswordVisible(false);
+  };
 
-	return (
-		<View style={styles.main}>
-			<Header color="#da291c" title="PROFIL" user={props.user} />
-			<Modal
-				animationType="fade"
-				transparent={true}
-				visible={modalPasswordVisible}
-				onRequestClose={() => {
-					setModalPasswordVisible(!modalPasswordVisible)
-				}}
-			>
-				<View style={modalStyle.modal}>
-					<View style={modalStyle.addPanel}>
-						<TextInput
-							style={modalStyle.input}
-							placeholder="Ancien mot de passe"
-							value={oldPassword}
-							onChangeText={setOldPassword}
-						/>
-                        <TextInput
-							style={modalStyle.input}
-							placeholder="Nouveau mot de passe"
-							value={newPassword}
-							onChangeText={setNewPassword}
-						/>
-						<TextInput
-							style={modalStyle.input}
-							placeholder="Confirmation du nouveau mot de passe"
-							value={confirmedPassword}
-							onChangeText={setConfirmedPassword}
-						/>
-						<TouchableOpacity
-							style={modalStyle.bt}
-							onPress={() => {}}
-						>
-							<Text style={modalStyle.textBT}>Valider</Text>
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={modalStyle.bt}
-							onPress={() => {
-								resetModalNewPassword()
-							}}
-						>
-							<Text style={modalStyle.textBT}>Annuler</Text>
-						</TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
-            <TouchableOpacity onPress={() => {
-                setModalPasswordVisible(true);
-            }}>
-                <AdminButton text="Changer de mot de passe" onPress={() => {}}/>
+  const changePassword = () => {
+    Api.post(
+      "change-password",
+      {
+        password: oldPassword,
+        new_password: newPassword,
+        new_password_confirm: confirmedPassword,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${props.token}`,
+        },
+      }
+    ).then((response) => {
+      resetModalNewPassword();
+    });
+  };
+
+  return (
+    <View style={styles.main}>
+      <Header color="#da291c" title="PROFIL" user={props.user} />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalPasswordVisible}
+        onRequestClose={() => {
+          setModalPasswordVisible(!modalPasswordVisible);
+        }}
+      >
+        <View style={modalStyle.modal}>
+          <View style={modalStyle.addPanel}>
+            <TextInput
+              style={modalStyle.input}
+              placeholder="Ancien mot de passe"
+              value={oldPassword}
+              onChangeText={setOldPassword}
+            />
+            <TextInput
+              style={modalStyle.input}
+              placeholder="Nouveau mot de passe"
+              value={newPassword}
+              onChangeText={setNewPassword}
+            />
+            <TextInput
+              style={modalStyle.input}
+              placeholder="Confirmation du nouveau mot de passe"
+              value={confirmedPassword}
+              onChangeText={setConfirmedPassword}
+            />
+            <TouchableOpacity style={modalStyle.bt} onPress={changePassword}>
+              <Text style={modalStyle.textBT}>Valider</Text>
             </TouchableOpacity>
-            <Navbar color="#da291c" user={props.user} />
+            <TouchableOpacity
+              style={modalStyle.bt}
+              onPress={resetModalNewPassword}
+            >
+              <Text style={modalStyle.textBT}>Annuler</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    )
-}
+      </Modal>
+      <TouchableOpacity
+        onPress={() => {
+          setModalPasswordVisible(true);
+        }}
+      >
+        <AdminButton text="Changer de mot de passe" onPress={() => {}} />
+      </TouchableOpacity>
+      <Navbar color="#da291c" user={props.user} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	main: {
-		flex: 1,
-		backgroundColor: "#F8F8F8",
-		overflow: 'hidden'
-	},
-})
+  main: {
+    flex: 1,
+    backgroundColor: "#F8F8F8",
+    overflow: "hidden",
+  },
+});
 
-export default Profil
+export default Profil;
